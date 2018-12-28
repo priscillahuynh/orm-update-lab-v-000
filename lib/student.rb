dccb4c01926c7873f39b3ba804f2c9ab136d52cf
Priscilla H <priscillamhuynh@gmail.com>
2018-12-28 12:08:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,4 +49,8 @@ class Student
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
+
+  def self.new_from_db
+    
+  end
 end
