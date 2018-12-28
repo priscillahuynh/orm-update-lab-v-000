afb1178e6b4bdd409d4acda648a0d77d62b2c692
Priscilla H <priscillamhuynh@gmail.com>
2018-12-28 12:22:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,14 +70,14 @@ class Student
     end.first
   end
 
-  def self.all
-    sql =<<-SQL
-    SELECT *
-    FROM students
-    SQL
-
-    DB[:conn].execute(sql).map do |row|
-    self.new_from_db(row)
-    end
-  end
+  # def self.all
+  #   sql =<<-SQL
+  #   SELECT *
+  #   FROM students
+  #   SQL
+  # 
+  #   DB[:conn].execute(sql).map do |row|
+  #   self.new_from_db(row)
+  #   end
+  # end
 end
