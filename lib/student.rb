6ee6c7c277a5c71e58a017b896d7c02c90557d8b
Priscilla H <priscillamhuynh@gmail.com>
2018-12-28 12:22:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,12 +71,13 @@ class Student
   end
 
   def self.all
-    sql =<<-SQL 
-    SELECT * 
-    FROM students 
-    SQL 
+    sql =<<-SQL
+    SELECT *
+    FROM students
+    SQL
 
     DB[:conn].execute(sql).map do |row|
     self.new_from_db(row)
+  end
   end
 end
